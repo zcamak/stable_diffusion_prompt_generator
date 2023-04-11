453c5482b46db9530f124a30531770d46e68fe6d
zcamak <krzysztofkaminski85@gmail.com>
2023-04-11 23:02:48 +0200

    Added sub-menu

--- run_generator.py
+++ run_generator.py
@@ -4,6 +4,48 @@ import json
 import parser
 import helper
 
+################################################################################
+# Functions
+
+def separator():
+    print("-" * helper.MAX_LENGTH)
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def main_menu():
+    clear()
+    helper.decorate_title("Category:")
+    for i, item in enumerate(data.keys()):
+        print(i, item)
+        keys.append(item)
+    separator()
+    print("[g] generate")
+    print("[q] quit")
+    separator()
+
+    return input("> Please select category:\n> ")
+
+def sub_menu(index):
+    clear()
+    helper.decorate_title(index.upper())
+    for i, item in enumerate(data[index].keys()):
+        print(i, item, " - ", data[index][item])
+        if (i + 1) % 20 == 0:
+            request = input("--- Press any key to continue or 'q' to quit ---")
+            if request == 'q':
+                break
+            clear()
+            helper.decorate_title(index.upper())
+
+    separator()
+    print("[q] quit")
+    separator()
+
+    return input("> Please select item:\n> ")
+
+################################################################################
+
 prompt, filename = parser.parse_input()
 
 if filename == None:
@@ -18,33 +60,52 @@ except FileNotFoundError:
 except Exception as e:
     print("[-] ERROR!: {e}")
     sys.exit()
+finally:
+    f.close()
+
+new_prompt = {}
 
 if prompt == None:
 
     while True:
         try:
-            os.system("cls" if os.name == "nt" else "clear")
             keys = []
-            helper.decorate_title("Category:")
-            for i, item in enumerate(data.keys()):
-                print(i+1, item)
-                keys.append(item)
-            print("-" * helper.MAX_LENGTH)
-            print("[g] generate")
-            print("[q] quit")
-            print("-" * helper.MAX_LENGTH)
-
-            category = input("> Please select category:\n> ")
+            category = main_menu()
 
             if category == 'q':
                 print("[+] Quitting...")
                 break
             elif category == 'g':
                 helper.decorate_title("PROMPT:")
+                prompt = ""
+                for item in new_prompt.values():
+                    prompt += item + ", "
                 print("\n", prompt)
                 input("\n> Press any key to continue...")
             else:
-                print("KEY: ", data[keys[int(category)]])
+                while True:
+                    requested = sub_menu(keys[int(category)])
+
+                    if requested == 'q':
+                        print("[+] Going back...")
+                        break
+                    elif requested.isnumeric() == True:
+                        datalength = len(data[keys[int(category)]]) 
+                        if int(requested) >= datalength:
+                            print(f"[-] Invalid number (max: {datalength})")
+                            input("\n> Press any key to continue...")
+                            continue
+                            
+                        category_key = keys[int(category)]
+                        for i, item in enumerate(data[category_key]):
+                            if i == int(requested):
+                                new_prompt[category_key] = item
+                                break
+                        input("\n> Press any key to continue...")
+                        break
+                    else:
+                        print("AAAAAAAA")
+                    input("\n> Press any key to continue...")
         except KeyboardInterrupt:
             print("[!] Program interrupted. Quitting...")
             break
