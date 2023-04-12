6f7d126395eb782855a87f66b252f3da8a2766a2
zcamak <krzysztofkaminski85@gmail.com>
2023-04-12 22:24:06 +0200

    Code cleanup

---
+++ modules/gpt.py
@@ -0,0 +1,19 @@
+import openai
+
+def prompt_gpt(apikey):
+    model = "gpt-3.5-turbo"
+    msg = [
+        {
+          "role": "user",
+          "content": "Create a subject for a prompt for AI image generator Stable Diffusion. It should be one sentence and no longer than 7 words."
+        }
+    ]
+
+    openai.api_key = apikey
+
+    print("[+] Waiting for GPT response...")
+    response = openai.ChatCompletion.create(model=model, messages=msg)
+    subject = response.choices[0].message.content
+    subject = subject.strip('"')
+
+    return subject
